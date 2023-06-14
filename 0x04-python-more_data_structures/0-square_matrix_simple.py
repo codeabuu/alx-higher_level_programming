f2f229c8e6ee53c433957332f4398bf2f585f9f8
codeabuu <abdulngui718@gmail.com>
2023-06-14 03:29:34 -0700

    sq value of matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     new_mat = matrix.copy()
 
     for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-    return (new_matrix)
+        new_mat[i] = list(map(lambda x: x**2, matrix[i]))
+    return (new_mat)
