76708da0109e0b425b7eafda3a83fd0ae09b8ec5
codeabuu <abdulngui718@gmail.com>
2023-06-14 03:26:36 -0700

    square value of all integers of a matrix.

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_mat = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+    return (new_matrix)
