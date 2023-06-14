a43051df75c126e8c2dc46d0cd6e3d0216fb0425
codeabuu <abdulngui718@gmail.com>
2023-06-14 05:01:01 -0700

    Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    total_score = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+
+    weighted_average = total_score / total_weight
+    return weighted_average
