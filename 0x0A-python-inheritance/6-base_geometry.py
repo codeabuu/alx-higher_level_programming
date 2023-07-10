17fa98a539693119b3520cad10b14bb906d20742
codeabuu <abdulngui718@gmail.com>
2023-07-10 05:11:40 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines parent class"""
+
+
+class BaseGeometry:
+    """parent class basegeometry"""
+    def def area(self):
+        """public instance method with and exception raised"""
+        raise Exception("area() is not implemented")
