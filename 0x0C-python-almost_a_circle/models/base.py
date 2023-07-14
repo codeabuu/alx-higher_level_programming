886a8bd3c8a13ac9c84720d0ca11f51ebfd31d6f
codeabuu <abdulngui718@gmail.com>
2023-07-14 04:51:22 -0700

    create our class base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
