cf317e3e419be8c3d4ed4a6bc027cf2a1efd5bbb
codeabuu <abdulngui718@gmail.com>
2023-07-14 05:21:24 -0700

    update on rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+from models.base import Base
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    def width(self):
+        """Returning private attribute"""
+        return self.__width
+    def width(self, value):
+        """Setting private attribute"""
+        self.setter_validation("width", value)
+        self.__width = value
+    def height(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("y", value)
+        self.__y = value
