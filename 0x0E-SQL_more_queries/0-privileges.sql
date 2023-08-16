30859535faa479c026a5a3423c9f5c5bb33724f0
codeabuu <abdulngui718@gmail.com>
2023-08-16 04:28:15 -0700

    script that lists all privileges of the MySQL users user_0d_1 and user_0d_2

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- list privilages for user 1 and 2
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
