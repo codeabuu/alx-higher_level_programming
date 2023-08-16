e3750c2800a1b454789455dad45cd7bc6afeaecc
codeabuu <abdulngui718@gmail.com>
2023-08-16 05:01:37 -0700

    updates

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- list privilages for user 1 and 2
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
 
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
