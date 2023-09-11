cb438cdb3ecdb2d6462c0e57d5aa4914367521b7
codeabuu <abdulngui718@gmail.com>
2023-09-11 07:08:38 -0700

    script that prints My number if the first argument can be converted to an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 // Get the first argument from the command line
 const arg = process.argv[2];
 
-//convert the argument to an integer
+// convert the argument to an integer
 const intValue = parseInt(arg);
 
 // Check if the result is not NaN (isNaN returns true for NaN)
