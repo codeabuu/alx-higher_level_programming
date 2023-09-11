9245f04c3e358933025db2bab2dbcf96c5c2290a
codeabuu <abdulngui718@gmail.com>
2023-09-11 07:30:40 -0700

    script that prints 3 lines

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const lines = [
-  "C is fun",
-  "Python is cool",
-  "JavaScript is amazing",
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 for (let i = 0; i < lines.length; i++) {
