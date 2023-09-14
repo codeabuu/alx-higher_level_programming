f48a03f8961d7c7f1fc58f2fdee60608964ad1a7
codeabuu <abdulngui718@gmail.com>
2023-09-14 05:00:20 -0700

    a safe script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
+"""
+
+import MySQLdb as db
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    conn = db.connect(host="localhost", port=3306,
+                      user=argv[1], passwd=argv[2], db=argv[3])
+    db_cursor = conn.cursor()
+    db_cursor.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY \
+                    %(name)s ORDER BY id ASC", {'name': argv[4]})
+    r_sel = db_cursor.fetchall()
+    for row in r_sel:
+        print(row)
