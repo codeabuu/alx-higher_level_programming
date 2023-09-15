1f05e4bcbe4be2f6e9c276eab3cd6af14aff88f9
codeabuu <abdulngui718@gmail.com>
2023-09-15 07:12:32 -0700

    updates

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 """
-Script to list all states from the dbase hbtn_0e_0_USA
+This script lists all states from the
+database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """
     Access to the database and get the states
     from the database.
     """
-    conn = MySQLdb.connect(
-            host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
-    db_cursor = conn.cursor()
-    db_cursor.execute("SELECT*FROM states")
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute("SELECT * FROM states")
 
     rows_selected = db_cursor.fetchall()
 
