3c40dd124520f9f68f63f53d3ca49632aa52926b
codeabuu <abdulngui718@gmail.com>
2023-09-29 04:49:26 -0700

    a Python script that fetches url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#Python script that fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode("utf-8")))
